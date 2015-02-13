6cd191dc6d375f4cb6f3a2e68b35e6f227fe93ff
midnitekiller <liefjillcolegado@gmail.com>
2015-02-14 05:34:56 +0800

    Updated

--- files/web_app/calibration.js
+++ files/web_app/calibration.js
@@ -1,13 +1,12 @@
 var myChar = (["abc",4,"hey"]);
-var myArray = ["grapes", "apples", "beer", "pizzas"];
 
 var logItem = function (item) {
-  console.log(item);
+	 console.log(item); 
 };
- 
+
 var theLast = function (list, action) {
-	var handle = list.toString();
-	return action(handle[--handle.length]);
+	 var handle = list.toString(); 
+	 return action(handle[--handle.length]);
 };
 
 theLast(myChar, logItem);
